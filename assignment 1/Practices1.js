4d6c927997ab656c8b490f902d0acb347aa6a0b4
ojbgigi <ojbgigi@gmail.com>
2023-05-06 14:39:16 +0800

    practices1 finish!

---
+++ assignment 1/Practices1.js
@@ -0,0 +1,62 @@
+// 1-1
+let sum = 10 + 7;
+console.log(sum);
+
+let difference = 10 - 7;
+console.log(difference);
+
+let product = 10 * 2;
+console.log(product);
+
+let quotient = 10 / 2;
+console.log(quotient);
+
+let remainder = 10 % 3;
+console.log(remainder);
+
+let increment = 5;
+increment++;
+console.log(increment);
+
+let decrement = 5;
+console.log(decrement);
+
+
+// 1-2
+let a = 10;
+let b = 5;
+
+let resaultSum = a + b;
+let resaultDifference = a - b;
+let resaultProduct = a * b;
+let resaultQuotient = a / b;
+let resaultRemainder = a % b;
+
+console.log(resaultSum);
+console.log(resaultDifference);
+console.log(resaultProduct);
+console.log(resaultQuotient);
+console.log(resaultRemainder);
+
+
+// 1-3
+let x = 10;
+let y = 3;
+let z = "3";
+
+console.log(x == y);
+console.log(x != y);
+console.log(x > y);
+console.log(x < y);
+console.log(x >= y);
+console.log(x <= y);
+console.log(y === z);
+console.log(y !== z);
+
+//1-4
+let aa = 10;
+let bb = 5;
+let cc = 3;
+console.log(aa > bb && bb < cc);
+console.log(aa > bb || bb < cc);
+console.log(!(aa > bb));
