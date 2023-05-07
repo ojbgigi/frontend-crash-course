2957595101dd198114ba717d6a0137d5dee4af2f
ojbgigi <ojbgigi@gmail.com>
2023-05-07 09:58:46 +0800

    Practices3 finish

---
+++ assignment 1/Practices3.js
@@ -0,0 +1,34 @@
+
+// 3-1
+let day = 3;
+switch (day) {
+    case 0: console.log("Sunday");
+        break;
+    case 1: console.log("Monday");
+        break;
+    case 2: console.log("Tuesday");
+        break;
+    case 3: console.log("Wednesday");
+        break;
+    case 4: console.log("Thursday");
+        break;
+    case 5: console.log("Friday");
+        break;
+    case 6: console.log("Saturday");
+        break;
+    default: console.log("This is not a valid day");
+}
+
+// 3-2
+let season = 4;
+switch (season) {
+    case 1: console.log("Spring");
+        break;
+    case 2: console.log("Summer");
+        break;
+    case 3: console.log("Fail");
+        break;
+    case 4: console.log("Winter");
+        break;
+    default: console.log("This is not a valid season");
+}
