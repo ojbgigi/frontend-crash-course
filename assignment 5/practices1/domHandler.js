20841c2623023ec5d02190888736c4a33003de28
ojbgigi <ojbgigi@gmail.com>
2023-06-06 20:19:31 +0800

    Course 5-practices1 完成

--- assignment 5/practices1/domHandler.js
+++ assignment 5/practices1/domHandler.js
@@ -1,11 +1,22 @@
 export const getNodes = () => {
-    const num1 = document.querySelector(".input-num1");
-    const num2 = document.querySelector(".input-num2");
-    const equals = document.querySelector(".equals-btn");
-    const operationBtn = document.querySelector(".operation");
-    // const plus = document.querySelector(".plus-btn");
-    // const minus = document.querySelector(".minus-btn");
-    // const multiply = document.querySelector(".multiply-btn");
-    // const divide = document.querySelector(".divide-btn");
-    return { num1, num2, equals, operationBtn }
-}
+  const num1 = document.querySelector(".input-num1");
+  const num2 = document.querySelector(".input-num2");
+  const equals = document.querySelector(".equals-btn");
+  const operationBtn = document.querySelector(".operation");
+  const result = document.querySelector(".result");
+  const plusBtn = document.querySelector("#plus-btn");
+  const minusBtn = document.querySelector("#minus-btn");
+  const multiplyBtn = document.querySelector("#multiply-btn");
+  const divideBtn = document.querySelector("#divide-btn");
+  return {
+    num1,
+    num2,
+    equals,
+    operationBtn,
+    result,
+    plusBtn,
+    minusBtn,
+    multiplyBtn,
+    divideBtn,
+  };
+};
--- assignment 5/practices1/index.js
+++ assignment 5/practices1/index.js
@@ -1,20 +1,55 @@
 import { getNodes } from "./domHandler.js";
 import { plus, minus, multiply, divide } from "./operation.js";
 
-const { num1, num2, equals, operationBtn } = getNodes();
+const {
+  num1,
+  num2,
+  equals,
+  operationBtn,
+  result,
+  plusBtn,
+  minusBtn,
+  multiplyBtn,
+  divideBtn,
+} = getNodes();
+
+let operationFn;
 
 operationBtn.addEventListener("click", (e) => {
-    console.log(e.target.className);
-})
-
+  plusBtn.removeAttribute("class", "focus");
+  minusBtn.removeAttribute("class", "focus");
+  multiplyBtn.removeAttribute("class", "focus");
+  divideBtn.removeAttribute("class", "focus");
+  console.log(e.target.id);
+  switch (e.target.id) {
+    case "plus-btn":
+      operationFn = plus;
+      plusBtn.setAttribute("class", "focus");
+      break;
+    case "minus-btn":
+      operationFn = minus;
+      minusBtn.setAttribute("class", "focus");
+      break;
+    case "multiply-btn":
+      operationFn = multiply;
+      multiplyBtn.setAttribute("class", "focus");
+      break;
+    case "divide-btn":
+      operationFn = divide;
+      divideBtn.setAttribute("class", "focus");
+      break;
+    default:
+      alert("請選擇如何運算!");
+      break;
+  }
+});
 
 equals.addEventListener("click", () => {
-    if (num1.value !== "" && num2.value !== "") {
-        let inputNum1 = Number(num1.value);
-        let inputNum2 = Number(num2.value);
-
-    } else {
-        alert("請輸入數字！")
-    }
-
-})
+  if (num1.value !== "" && num2.value !== "") {
+    let inputNum1 = Number(num1.value);
+    let inputNum2 = Number(num2.value);
+    result.textContent = operationFn(inputNum1, inputNum2);
+  } else {
+    alert("請輸入數字！");
+  }
+});
